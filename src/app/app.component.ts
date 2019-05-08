bcf89a7f8af523ae86a3f4d14a2222c341e9aac8
nasriime <mnasr4040@gmail.com>
2019-05-08 03:21:16 +0200

    add forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'inovation';
+export class AppComponent implements OnInit{
+  loginForm: FormGroup;
+  registrationForm: FormGroup;
+  login = true;
+  registration = false;
+  loading = false;
+  submitted = false;
+  error = '';
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private AuthenticationService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+
+    this.registrationForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: [''],
+      number: ['',[
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+        Validators.minLength(8)
+      ]],
+      address: ['', Validators.required],
+      authentication: ['Password'],
+      password: ['', Validators.required]
+    });
+
+    this.conditionalNumber();
+  }
+
+  get l() { return this.loginForm.controls; }
+  get r() { return this.registrationForm.controls; }
+
+  onLoginSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+        return;
+    }
+
+    this.loading = true;
+}
+
+onRegistrationSubmit() {
+  this.submitted = true;
+
+  // stop here if form is invalid
+  if (this.registrationForm.invalid) {
+      return;
+  }
+
+  this.loading = true;
+  
+}
+
+conditionalNumber() {
+  const number = this.registrationForm.get('number');
+
+  this.registrationForm.get('email').valueChanges
+    .subscribe(email => {
+
+      if (email == '') {
+        number.setValidators([Validators.required]);
+      }
+
+      if (email.length > 0) {
+        number.setValidators(null);
+      }
+
+      number.updateValueAndValidity();
+    });
+}
+
+
 }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  public getUser(): Observable<any> {
+    return this.http.get("/menu.json");
+  }
+}
